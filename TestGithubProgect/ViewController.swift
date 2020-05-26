560f1accfc421e870195cfeb26fe38ee2af11af9
Ilia Klioukin <il2be97@gmail.com>
2020-05-26 18:58:21 +0300

    Initial commit

---
+++ TestGithubProgect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGithubProgect
+//
+//  Created by Илья Клюкин on 5/26/20.
+//  Copyright © 2020 Илья Клюкин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
